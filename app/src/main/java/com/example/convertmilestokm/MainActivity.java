fc7f5bbc3c4af04f81ca9d53c9c03d5225f1c2f3
harshkumar-hacktoberfest <72377904+harshkumar-hacktoberfest@users.noreply.github.com>
2020-10-05 14:50:14 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/convertmilestokm/MainActivity.java
+++ app/src/main/java/com/example/convertmilestokm/MainActivity.java
@@ -23,7 +23,31 @@ public class MainActivity extends AppCompatActivity {
         */
         
         
+        Button buttonConvMilesToKm = (Button)findViewById(R.id.buttonConvMilesToKm);
+        buttonConvMilesToKm.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+        EditText textBoxMiles = (EditText)findViewById(R.id.editTextMiles);
+        EditText textBoxKm = (EditText)findViewById(R.id.editTextKm);
+        double vMiles = Double.valueOf(textBoxMiles.getText().toString());
+        double vKm = vMiles / 0.62137;
+        DecimalFormat formatVal = new DecimalFormat("##.##");
+        textBoxKm.setText(formatVal.format(vKm));
+    }
+});
         
+    Button buttonConvKmToMiles = (Button)findViewById(R.id.buttonConvKmToMiles);
+    buttonConvKmToMiles.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        EditText textBoxMiles = (EditText)findViewById(R.id.editTextMiles);
+        EditText textBoxKm = (EditText)findViewById(R.id.editTextKm);
+        double vKm = Double.valueOf(textBoxKm.getText().toString() );
+        double vMiles = vKm * 0.62137;
+        DecimalFormat formatVal = new DecimalFormat("##.##");
+        textBoxMiles.setText(formatVal.format(vMiles));
+    }
+});
         
         
         
